d4d3a515eb768b868ddbb0dee5e91c729049d5b2
TheBoos2569 <32793818+TheBoos2569@users.noreply.github.com>
2017-12-11 00:14:08 +0200

    Added Moovale fixes

--- expansions/script/c10505098.lua
+++ expansions/script/c10505098.lua
@@ -1,51 +1,50 @@
---Moonvale Surge
-function c10505091.initial_effect(c)
-	--add
+--Moonvale Guidance
+function c10505098.initial_effect(c)
+--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetCategory(CATEGORY_TOGRAVE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,10505091+EFFECT_COUNT_CODE_OATH)
-	e1:SetOperation(c10505091.activate)
+	e1:SetCountLimit(1,10505098)
+	e1:SetOperation(c10505098.activate)
 	c:RegisterEffect(e1)
-	--Boost
+	--extra summon
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_UPDATE_ATTACK)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+	e2:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
 	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x1a4))
-	e2:SetValue(500)
 	c:RegisterEffect(e2)
 	--Ritual Summon
 	local e4=Effect.CreateEffect(c)
 	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e4:SetDescription(aux.Stringid(10505091,1))
+	e4:SetDescription(aux.Stringid(10505098,1))
 	e4:SetType(EFFECT_TYPE_IGNITION+EFFECT_TYPE_QUICK_O)
 	e4:SetRange(LOCATION_SZONE)
-	e4:SetCost(c10505091.spcost)
-	e4:SetTarget(c10505091.target)
-	e4:SetOperation(c10505091.operation)
+	e4:SetCost(c10505098.spcost)
+	e4:SetTarget(c10505098.target)
+	e4:SetOperation(c10505098.operation)
 	c:RegisterEffect(e4)
 end
-function c10505091.thfilter(c)
-	return c:IsLevelBelow(4) and c:IsSetCard(0x1a4) and c:IsType(TYPE_MONSTER) and not c:IsType(TYPE_RITUAL) and c:IsAbleToHand()
+function c10505098.tgfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x1a4) and c:IsAbleToGrave()
 end
-function c10505091.activate(e,tp,eg,ep,ev,re,r,rp)
+function c10505098.activate(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local g=Duel.GetMatchingGroup(c10505091.thfilter,tp,LOCATION_DECK,0,nil)
-	if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(10505091,0)) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.GetMatchingGroup(c10505098.tgfilter,tp,LOCATION_DECK,0,nil)
+	if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(10505098,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 		local sg=g:Select(tp,1,1,nil)
-		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.SendtoGrave(sg,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,sg)
 	end
 end
-function c10505091.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c10505098.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
 	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
 end
-function c10505091.filter(c,e,tp,m,ft)
+function c10505098.filter(c,e,tp,m,ft)
 	if not c:IsSetCard(0x1a4) or bit.band(c:GetType(),0x81)~=0x81
 		or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true) then return false end
 	local mg=m:Filter(Card.IsCanBeRitualMaterial,c,c)
@@ -55,28 +54,28 @@ function c10505091.filter(c,e,tp,m,ft)
 	if ft>0 then
 		return mg:CheckWithSumGreater(Card.GetRitualLevel,c:GetLevel(),c)
 	else
-		return ft>-1 and mg:IsExists(c10505091.mfilterf,1,nil,tp,mg,c)
+		return ft>-1 and mg:IsExists(c10505098.mfilterf,1,nil,tp,mg,c)
 	end
 end
-function c10505091.mfilterf(c,tp,mg,rc)
+function c10505098.mfilterf(c,tp,mg,rc)
 	if c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) then
 		Duel.SetSelectedCard(c)
 		return mg:CheckWithSumGreater(Card.GetRitualLevel,rc:GetLevel(),rc)
 	else return false end
 end
-function c10505091.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function c10505098.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local mg=Duel.GetRitualMaterial(tp)
 		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-		return Duel.IsExistingMatchingCard(c10505091.filter,tp,LOCATION_HAND,0,1,nil,e,tp,mg,ft)
+		return Duel.IsExistingMatchingCard(c10505098.filter,tp,LOCATION_HAND,0,1,nil,e,tp,mg,ft)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
-function c10505091.operation(e,tp,eg,ep,ev,re,r,rp)
+function c10505098.operation(e,tp,eg,ep,ev,re,r,rp)
 	local mg=Duel.GetRitualMaterial(tp)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tg=Duel.SelectMatchingCard(tp,c10505091.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp,mg,ft)
+	local tg=Duel.SelectMatchingCard(tp,c10505098.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp,mg,ft)
 	local tc=tg:GetFirst()
 	if tc then
 		mg=mg:Filter(Card.IsCanBeRitualMaterial,tc,tc)
@@ -90,7 +89,7 @@ function c10505091.operation(e,tp,eg,ep,ev,re,r,rp)
 			Duel.ReleaseRitualMaterial(mat)
 		else
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-			mat=mg:FilterSelect(tp,c10505091.mfilterf,1,1,nil,tp,mg,tc)
+			mat=mg:FilterSelect(tp,c10505098.mfilterf,1,1,nil,tp,mg,tc)
 			Duel.SetSelectedCard(mat)
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
 			local mat2=mg:SelectWithSumGreater(tp,Card.GetRitualLevel,tc:GetLevel(),tc)
