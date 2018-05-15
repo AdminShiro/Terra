ef8d33f3fc9452e13fdf4596960f74cb738320a9
YGOCC <chadoook1990@gmail.com>
2018-05-15 20:15:16 +0100

    Fixes:ESPergear and Lana
    Major Qced ESPergear fixes
    And Lana got an Errata.

--- expansions/script/c160008874.lua
+++
@@ -1,5 +0,0 @@
---Cyberse Jumper
-function c160008874.initial_effect(c)
-	aux.AddLinkProcedure(c,nil,2)
-	c:EnableReviveLimit()	
-end
--- expansions/script/c50031786.lua
+++ expansions/script/c50031786.lua
@@ -1,8 +1,9 @@
 --Lana, Whiptail Girl of Magnificent VINE
 function c50031786.initial_effect(c)
 	 --link summon
-	aux.AddLinkProcedure(c,nil,2,2)
 	c:EnableReviveLimit()
+	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsLinkSetCard,0x85a),2,2)
+ 
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(50031786,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -61,12 +62,13 @@ function c50031786.target2(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 
-function c50031786.filter(c,e,tp)
-	return c:IsSetCard(0x85a) and c:IsLevelBelow(5) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function c50031786.filter(c,e,tp,zone)
+	return c:IsSetCard(0x85a) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp,zone)
 end
 function c50031786.operation(e,tp,eg,ep,ev,re,r,rp)
 local zone=e:GetHandler():GetLinkedZone()
 	  if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	if zone==0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,c50031786.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp,zone)
 	local tc=g:GetFirst()
